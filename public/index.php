feec72f68eb9fc32ee8d18e9d2f60f52aeffc968
Leerlandais <leerlandais@gmail.com>
2024-02-04 15:57:13 +0100

    getting started

---
+++ public/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DSP Spreadsheet</title>
+    <link rel="stylesheet" href="/public/styles/style.css">
+</head>
+<body>
+<section id = "header">
+<h1>DSP Spreadsheet Maker</h1>
+</section>
+
+<hr>
+<section id = "buttons">
+    <button class="button" id="addButton">Add System</button>
+    <button class="button" id="remButton"></button>
+    <button class="button"></button>
+    <button class="button"></button>
+    <button class="button"></button>
+    <button class="button"></button>
+</section>
+<hr>
+<section id = "mainBody">
+    
+</section>
+<section id = "footer">
+<hr>
+<h4 id="copyright">&copy; Lee Brennan
+</h4>
+<hr>  
+</section>
+
+    <script src="/public/scripts/script.js"></script>
+</body>
+</html>
