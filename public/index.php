6cb529970685a2b38b77b5b8e808809d898aeefa
leerlandais <leerlandais@gmail.com>
2024-02-05 12:10:43 +0100

    added stuff

--- public/index.php
+++ public/index.php
@@ -7,6 +7,26 @@
     <link rel="stylesheet" href="/public/styles/style.css">
 </head>
 <body>
+    <?php
+    require_once "../config.php";
+
+    try {
+        $db = new PDO(
+            MY_DB_TYPE.':host='.MY_DB_HOST.';port='.MY_DB_PORT.';dbname='.MY_DB_NAME.';charset='.MY_DB_CHARSET,
+            MY_DB_LOGIN,
+            MY_DB_PWD
+        );
+            $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);   // default method for FETCH
+    }catch(Exception $e){
+        die($e->getMessage());
+    }
+    $showSys = "SELECT * from systemnames";
+    $query = $db->query($showSys);
+    $allSys = $query->fetchAll();
+    $planCount = "SELECT planetCount from systemnames";
+    $planQuery = $db->query($planCount);
+    foreach ($allSys as $systems):
+    ?>
 <section id = "header">
 <h1>DSP Spreadsheet Maker</h1>
 </section>
@@ -22,7 +42,14 @@
 </section>
 <hr>
 <section id = "mainBody">
-    <h4 id = "sysName"></h4><h4 class="planetName"></h4>
+    <table id="mainTable">
+        <tr>
+            <th>System Name</th>
+            <th>Planets</th>
+        </tr>
+        <tr id="innerTable">
+        </tr>
+</table>
 </section>
 <section id = "footer">
 <hr>
@@ -30,7 +57,7 @@
 </h4>
 <hr>  
 </section>
-
+<?php endforeach;?>
     <script src="/public/scripts/script.js"></script>
 </body>
 </html>
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,19 +1,63 @@
 let addBut = document.getElementById("addButton");
-let sysName = document.getElementById("sysName");
-let planetName = document.getElementById("planetName");
-
+let tableBody = document.getElementById("innerTable");
+const placeTabs = document.createElement("tr");
+const placeList = document.createElement("td");
+let romNums = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII"];
 addBut.addEventListener("click", addSystem);
 
 function addSystem(){
     var newSysName = prompt("Enter the name of your system");
-    var newPlanets = prompt("How many planets?");
-    let testEntry = /d/.test(newPlanets);
-    if (testEntry === false) {
-        newPlanets = prompt("How many planets?");   
-    }else {
-    }
-    console.log(testEntry);
-
-    sysName.textContent = newSysName
-    console.log(newSysName, " : ", newPlanets);
+   if (/['a-z']/.test(newSysName)){
+       tableBody.appendChild(placeTabs);
+       tableBody.appendChild(placeList);
+       tableBody.textContent=(newSysName);
+       console.log(newSysName);
+       planetCount(newSysName);
+}else {
+    addSystem();
 }
+}
+    function planetCount(newSysName){
+        var planetCount = prompt('How many planets in '+newSysName);
+        if (isNaN(planetCount || planetCount >=9)){
+            alert("Please enter a number from 1 - 8");
+            planetCount(newSysName);
+        }else {
+            console.log(planetCount);
+            for (i = 0; i < planetCount; i++){
+                tableBody.appendChild(placeList);
+                placeList.textContent = (newSysName+romNums[i]);
+            }
+        }
+}
+    
+
+
+/*
+    for (i = 1; i <= newPlanets; i++){
+        sysPlanets.push(newSysName+" "+romNums[i - 1]);
+        const placePlanets = document.createElement("td");
+        planetList.appendChild(placePlanets)
+        planetList.classList.add("planetList");
+        planetList.id = "list"+i; 
+        planetList.textContent = sysPlanets[i];
+    }
+}
+var newPlanets = prompt("How many planets?");
+    if(isNaN(newPlanets || newPlanets >= 9)){
+        alert("Please enter a number from 1 - 8");
+        planetCount(newSysName);
+        
+    }else {
+        const placeTabs = document.createElement("tr");
+        const placeList = document.createElement("td");
+        tableBody.appendChild(placeTabs)
+        tableBody.id = "planetTab";
+        var planetList = document.getElementById("planetTab");
+            for (i = 1; i < newPlanets; i++){
+                planetList.appendChild(placeList); 
+                planetList.id = "planList"+i;
+            }
+    
+
+*/
